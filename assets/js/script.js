1d754e9b7f2dcc9e3c891e39ad237ad9eb3f28df
MariaHoulihan <mariahoulihan@gmail.com>
2023-02-26 22:05:05 +0000

    Add quiz container,  start, next and option buttons

---
+++ assets/js/script.js
@@ -0,0 +1,83 @@
+const questions = [
+    { 
+      question: 'The occipital area of the skull is located in what part of the skull?',
+    answers: [
+      { text: 'Front', correct: false },
+      { text: 'Back', correct: true },
+      { text: 'Top', correct: false }
+      ]
+    },
+  {
+    question: 'The biceps femoris muscle is located in what part of the body?',
+    answers: [
+      { text: 'Shoulder', correct: false },
+      { text: 'Arm', correct: false },
+      { text: 'Leg', correct: true }
+    ]
+  },    
+    {
+    question: 'The elbow is an example of what type of joint?',
+    answers: [
+      { text: 'Pivot', correct: false },
+      { text: 'Hinge', correct: true },
+      { text: 'Saddle', correct: false }
+    ]
+  },  
+      {
+        question: 'The muscles located at the front of the thigh are known as what?',
+      answers: [
+        { text: 'Adductors', correct: false}, 
+        { text: 'Hamstrings', correct: false}, 
+        { text: 'Quadriceps', correct: true}
+      ]
+     },
+     {
+      question: 'The C1 vertebra is called?',
+      
+      answers: [
+        { text: 'Atlas', correct: true },
+        { text: 'Axis', correct: false },
+        { text: 'Amino', correct: false }
+      ]
+          }, 
+     {
+      question: 'The three bones that form the ankle are the fibula, tibia and which other bone?',
+      answers: [
+        { text: 'Talus', correct: true },
+        { text: 'Calcaneous', correct: false },
+        { text: 'Cuboid', correct: false }
+      ]
+     },
+     {
+       question: 'Which vessel returns unoxygenated blood to the lungs?',
+       answers: [
+         { text: 'Aorta', correct: false },
+         { text: 'Pulmonary artery', correct: true },
+         {text: 'Pulmonary vein', correct: false }
+    ]
+     }, 
+    {
+      question: 'Ligaments and Tendons are composed of what type of tissue?',
+        answers: [
+          { text: 'Epithelial', correct: false }, 
+          { text: 'Adipose', correct: false },
+          { text: 'Connective', correct: true }
+      ]
+      },
+    {
+      question: 'The anatomical term for the head is?',
+      answers: [
+        { text: 'Cephalic', correct: true },
+        { text: 'Cervical', correct: false },
+        { text: 'Thoracic', correct: false }
+      ]
+       }, 
+    {
+      question: 'Mastication is the term for what?',
+      answers: [
+        { text: 'Chewing', correct: true },
+        { text: 'Snoring', correct: false },
+        { text: 'Swallowing', correct: false }
+      ]
+        }  
+      ];
